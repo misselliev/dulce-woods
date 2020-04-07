8247dcd4df14a591d20371a0468e5d0c252e6617
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-04-07 11:52:46 -0700

    Adding About component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import React from 'react';
 import './styles/index.css';
 import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <About />
     </div>
   );
 }
