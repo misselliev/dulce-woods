487d455dd51da7e5c182c2f870b4072b9896bd88
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-04-15 14:31:52 -0700

    Creating Banner component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import wood from './wood.jpeg';
+
+const Banner = () => (
+  <img className="bg-cover bg-center" style={{backgroundImage:"url(https://images.unsplash.com/photo-1553729218-9ff2ede741f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80)", height:"20em"}} alt="banner"></img>
+);
+
+export default Banner;
