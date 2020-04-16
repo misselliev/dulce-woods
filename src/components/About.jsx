0d93ce7826372cc6fd39daac03706e0eb29f13ab
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-04-15 17:29:15 -0700

    Updating styling and making gallery cards responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => (
   <section id="about">
-    <div className="w-48 mx-auto pt-2 mb-6 border-b-2 border-purple-200 text-center text-yellow-900 pt-1 cursive">About</div>
+    <div className="w-48 mx-auto pt-4 mb-6 border-b-2 border-yellow-500 text-center text-yellow-900 pt-1 cursive">About</div>
     <div id="info" className="text-center text-yellow-700 mb-8">
       Designing beautiful wooden furniture to bring nature in your home in the most exquisite way.
       Since 1930.
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,11 +5,13 @@ import chair from './armlesschair.jpg'
 import bar from './woodenbar.jpg'
 const Gallery = () => (
   <section id="showcase">
-    <div className="w-48 mx-auto pt-2 mb-6 border-b-2 border-purple-200 text-center text-yellow-900 cursive">Showcase</div>
-    <div className="flex items-center justify-center m-8">
-      <Card title="Round dining set" description="Small nogal round dining set. 4 chairs included." imagesrc={roundset} imagealt="round dining set" tag1="dining" tag2="set" tag3="pine" />
-      <Card title="Armless lounge chair" description="Relaxed lounge armless chair, pairs perfect with a Serpian Premium Rug." imagesrc={chair} imagealt="armless chair" tag1="lounge" tag2="chair" tag3="pine" />
-      <Card title="Wooden library and bar set" description="Embedded library and bar set. 2 bar chairs included." imagesrc={bar} imagealt="library and bar set" tag1="library" tag2="bar" tag3="set" />
+    <div className="w-48 mx-auto pt-2 mb-4 border-b-2 border-yellow-500 text-center text-yellow-900 cursive">Showcase</div>
+    <div className="container mx-auto">
+      <div className="flex items-center flex-wrap justify-center">
+        <Card title="Round dining set" description="Small nogal round dining set. 4 chairs included." imagesrc={roundset} imagealt="round dining set" tag1="dining" tag2="set" tag3="pine" />
+        <Card title="Armless lounge chair" description="Relaxed lounge armless chair, pairs perfect with a Serpian Premium Rug." imagesrc={chair} imagealt="armless chair" tag1="lounge" tag2="chair" tag3="pine" />
+        <Card title="Wooden library and bar set" description="Embedded library and bar set. 2 bar chairs included." imagesrc={bar} imagealt="library and bar set" tag1="library" tag2="bar" tag3="set" />
+      </div>
     </div>
   </section>
 );
