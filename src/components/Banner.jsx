3f31cf3e4665d0f1da400ac7b67266828889cc61
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-04-15 17:01:52 -0700

    Updating screenshot and banner image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import wood from './wood.jpeg';
 
 const Banner = () => (
-  <img className="bg-cover bg-center" style={{backgroundImage:"url(https://images.unsplash.com/photo-1553729218-9ff2ede741f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80)", height:"20em"}} alt="banner"></img>
+  <img className="bg-cover bg-center" style={{ backgroundImage: 'url(https://images.unsplash.com/photo-1553729218-9ff2ede741f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80)', height: '15em' }} alt="banner" />
 );
 
 export default Banner;
